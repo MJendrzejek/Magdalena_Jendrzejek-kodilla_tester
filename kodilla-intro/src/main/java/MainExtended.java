2f0eecce3c963e23ea68e1f42f925da2b601526d
Magdalena Jendrzejek <magdalena.jendrzejek@gmail.com>
2022-04-27 15:09:18 +0200

    Main Extended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String game = "Lost Ark";
+        double itemLevel = 1370.52;
+        boolean mainCharacter = true;
+        char avatarClass = 'S';
+                //S is Shadowhunter//
+        int numberOfCharacters = 5;
+        System.out.println(itemLevel);
+    }
+}
